555f79a32a5ebe4c89fa4c3b8b53735cd17b2074
Peter Schmidt <peter@msdigest.net>
2019-02-25 11:17:35 +0100

    Update Export-IPs-from-ReceiveConnector.ps1

--- Export-IPs-from-ReceiveConnector.ps1
+++ Export-IPs-from-ReceiveConnector.ps1
@@ -1,1 +1,2 @@
-
+#Run this command to Export IP Range from the Receive Connector:
+Get-ReceiveConnector "servername\nameofconnector" | select -ExpandProperty remoteipranges | export-csv c:\temp\filename.csv
