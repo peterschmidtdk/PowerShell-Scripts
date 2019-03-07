fe630f463fabc0c6a6f346b85ec5742b981060c9
Peter Schmidt <peter@msdigest.net>
2019-03-07 12:48:30 +0100

    Create Set-MeetingRoom-DisplayOrganizer.ps1
    For EXO, set Rooms to Limited Details and show Organizer of the meeting in the details.

---
+++ Set-MeetingRoom-DisplayOrganizer.ps1
@@ -0,0 +1,20 @@
+# Author: Peter Schmidt
+# Blog: www.msdigest.net
+# Date: 2019.03.07 - Initial script
+# Will set Rooms to Limited Details and show Organizer of the meeting in the details
+#
+#Get EXO Credentials
+$credObject = Get-Credential
+
+#Logon to EXO
+$ExchOnlineSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $credObject -Authentication Basic -AllowRedirection
+Import-PSSession $ExchOnlineSession
+
+#Get all Rooms
+$rooms = Get-Mailbox -RecipientTypeDetails RoomMailbox
+
+#Set the AccessRights to Limited Details
+$rooms | %{Set-MailboxFolderPermission $_":\Calendar" -User Default -AccessRights LimitedDetails}
+ 
+#Set that Organizer are displayed the meetings details
+$rooms | %{Set-CalendarProcessing $_ -AddOrganizerToSubject $true -DeleteComments $false -DeleteSubject $false}
