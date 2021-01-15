9f4edf4b525a32d4364c0114c8ddfc866115e07f
Peter Schmidt <peter@msdigest.net>
2021-01-15 11:07:14 +0100

    Update AddUserstoAzureADGroup.ps1

--- AddUserstoAzureADGroup.ps1
+++ AddUserstoAzureADGroup.ps1
@@ -1,4 +1,5 @@
 #This small script adds users from a CSV list, listed with UserPrincipalName from the Azure AD Group specified, find the ObjectID for the Group
+#Connect with PowerShell to Azure AD
 
 #Change the GroupObject with the ObjectID of the group that users should be added to
 $GroupObject = "40xxx461-fxxb-4xx8-a6x2-0xxxxxxxx"
