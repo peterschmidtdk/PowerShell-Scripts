05ac3bcefe19463b12eae753b6aba1cba2af1a78
Peter Schmidt <peter@msdigest.net>
2021-02-04 09:06:52 +0100

    Update Set-PasswordToNeverExpire.ps1

--- Set-PasswordToNeverExpire.ps1
+++ Set-PasswordToNeverExpire.ps1
@@ -4,5 +4,11 @@
 #Using the Azure AD cmdlets:
 Get-AzureADUser -SearchString user@domain.com | Set-AzureADUser -PasswordPolicies DisablePasswordExpiration
 
+#Get list of users with Password Expire:
+Get-AzureADUser | ? {$_.PasswordPolicies -match "DisablePasswordExpiration"}
+
 #Using the MSOL cmdlets
 Set-MsolUser -UserPrincipalName user@domain.com -PasswordNeverExpires $true
+
+#Get list of users with Password Expire:
+Get-MsolUser -All | Set-MsolUser -PasswordNeverExpires $true
