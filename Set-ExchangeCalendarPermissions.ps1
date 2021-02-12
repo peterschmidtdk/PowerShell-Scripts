f2913ea637f6efda80805ee19ace9418047628ed
Peter Schmidt <peter@msdigest.net>
2021-02-12 11:17:07 +0100

    Update Set-ExchangeCalendarPermissions.ps1

--- Set-ExchangeCalendarPermissions.ps1
+++ Set-ExchangeCalendarPermissions.ps1
@@ -1,2 +1,6 @@
 #Set delegate calendar permissions on a mailbox, can be used for Exchange and in Exchange Online
 Set-MailboxFolderPermission JohnDoe:\calendar -User PeterSchmidt -AccessRights Editor -SharingPermissionFlags Delegate
+
+#This command sets permissions so the delegate can also see Private Items
+Set-MailboxFolderPermission JohnDoe:\calendar -User PeterSchmidt -AccessRights Editor -SharingPermissionFlags Delegate,CanViewPrivateItems
+
