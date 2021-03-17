222bf8ece214396132cb8ed4514e422abd6e4a66
Peter Schmidt <peter@msdigest.net>
2021-03-17 09:46:25 +0100

    Update List-AllMembersOfADistributionList.ps1

--- List-AllMembersOfADistributionList.ps1
+++ List-AllMembersOfADistributionList.ps1
@@ -1,2 +1,6 @@
 # This list all members of a specific Distribution List
-# Change the DL name, to the DL, which you want to list members from
+# Change the DL name (DLName), to the DL, which you want to list members from e.g. dist1@domain.com
+# It will display the: DisplayName, PrimarySmtpAddress for each members
+
+$Members = (Get-DistributionGroupMember DLName).Identity
+Foreach ($member in $members) { Get-Mailbox $Member | select DisplayName,PrimarySMTPAddress}
