2b1331ad5d8ce0159e83fe2c64d19d641329120b
Peter Schmidt <peter@msdigest.net>
2021-12-02 15:51:31 +0100

    Update Get-GuestMailUsers.ps1

--- Get-GuestMailUsers.ps1
+++ Get-GuestMailUsers.ps1
@@ -1,2 +1,4 @@
+# Need to connect to Exchange Online before running.
+# This script will show all Guest MailUsers and export the values to CSV, so it is possible to see if they are visible in the Exchange GAL.
 
 Get-MailUser -ResultSize unlimited | where { ($_.RecipientTypeDetails -eq "GuestMailUser") -and ($_.UserPrincipalName -like "*EXT*")} | Select Name,UserPrincipalName,HiddenFromAddress*,*smtp* | Export-Csv .\AllGuestUsers.csv -NoTypeInformation -Delimiter ";" -Encoding Unicode
